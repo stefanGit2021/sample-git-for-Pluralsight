f1c13e48dbf782fd67a12f55f77ad0f2e7f942ab
Stefan <stefan.marinescu@crystal-system.eu>
2021-03-16 13:52:03 +0200

    Add getter and setter for the name field

--- src/org/stefan/model/User.java
+++ src/org/stefan/model/User.java
@@ -1,5 +1,15 @@
 package org.stefan.model;
 
 public class User {
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
 
 }
